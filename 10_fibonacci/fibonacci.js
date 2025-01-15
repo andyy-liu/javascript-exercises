1cf540c7b5df902b06364e67c48ce3c7be13a0df
andyliu pc <andyliu3411@gmail.com>
2025-01-15 11:16:03 -0500

    completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    const fib = [0, 1]
+    
+    for (let i = 2; i <= input; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2]
+    }
 
+    return fib[input]
 };
 
 // Do not edit below this line
