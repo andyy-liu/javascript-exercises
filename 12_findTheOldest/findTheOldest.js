82380591615464cae252763761c407a3fc557475
andyliu pc <andyliu3411@gmail.com>
2025-01-15 11:47:01 -0500

    completed 12-findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people.sort((a, b) => {
+        let lastPersonAge = a.yearOfDeath - a.yearOfBirth
+        let nextPersonAge = b.yearOfDeath - b.yearOfBirth
+        return lastPersonAge < nextPersonAge ? 1 : -1
+    })
+    return oldest[0]
 };
 
 // Do not edit below this line
