f140821953fac642e49560f44219f457019e7947
andyliu pc <andyliu3411@gmail.com>
2025-01-15 11:03:55 -0500

    completed 09-palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (inputStr) {
+    // let stripped = str.replace(/[^\w\s\']|_/g, "").replace(/\s+/g, "").toLowerCase()
+    // let reverse = stripped.split("").reverse().join("")
+    // if (reverse === stripped) {
+    //     return true
+    // } else {
+    //     return false
+    // }
+    let alphanumeric = "abcdefghijklmnopqrstuvwxyz0123456789"
 
+    let cleanedString = inputStr.toLowerCase()
+                                .split('')
+                                .filter((character) => alphanumeric.includes(character))
+                                .join('')
+    
+    let reversedString = cleanedString.split('').reverse().join('')
+
+    return reversedString === cleanedString
 };
 
 // Do not edit below this line
